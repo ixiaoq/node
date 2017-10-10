99ab764dc42843bc3cd5039019f0414f81e32242
unknown <@txooo.co>
2017-10-10 16:03:14 +0800

    爬虫

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
-const http = require("https");
+const http = require("http");
 const cheerio = require("cheerio");
+const fs = require("fs");
 
-const url = "https://search.jd.com/Search?keyword=iphone&wq=iphone";
+const url = "http://www.dataoke.com/";
+let result = [];
 
-http.get(url, function(res){
-  res.on("data", function(data){
-    console.log("data" + data);
-  });
-  res.on("end", function(data){
-    console.log("end" + data);
-  });
+http.get(url, function (res) {
+	console.log('statusCode:', res.statusCode);
+
+	let html = "";
+
+	res.on("data", function (data) {
+		html += data;
+	});
+
+	res.on("end", function (data) {
+		var $ = cheerio.load(html);
+		$(".quan_goods").each((i, item) => {
+			var tempData = {
+				img: $(item).find(".lazy").attr("data-original"),
+				w: $(item).find(".lazy").attr("width"),
+				h: $(item).find(".lazy").attr("height"),
+				title: $(item).find(".quan_title").text(),
+				sell: $(item).find(".index_sell").find("span").text(),
+				saleMoney: $(item).find(".quan_sale_money").text(),
+				oldPrice: $(item).find(".quan_old_price").text().replace("￥", "")
+			};
+			//console.log(tempData);
+			result.push(tempData);
+		});
+
+		console.log("请求完成");
+
+		fs.writeFile("data.txt", JSON.stringify(result), (err) => {
+			if(err) return console.log(err);
+			console.log("写入成功");
+		});
+	});
+
+}).on("error", (error) => {
+	console.log(error);
 });
